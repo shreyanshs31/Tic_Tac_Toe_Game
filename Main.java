4dc05863adb67685e8ec0a3302269fc815e9c07d
Shreyansh Shukla <95318208+shreyanshs31@users.noreply.github.com>
2023-04-15 19:51:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -136,6 +136,7 @@ public class Main {
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
                 System.out.print(board[i][j]);
+                System.out.print(" ");
             }
             System.out.println();
         }
